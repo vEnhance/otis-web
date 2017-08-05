40abdef961ea59a4b256ddb32ee9e068207656bf
Evan Chen <evanchen@mit.edu>
2017-08-05 18:46:08 -0500

    Admin interface for exams

--- exams/admin.py
+++ exams/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+import exams
+
 # Register your models here.
+
+@admin.register(exams.models.MockOlympiad)
+class MockOlympiadAdmin(admin.ModelAdmin):
+	list_display = ('family', 'number',)
+	search_fields = ('family', 'number',)
+
+@admin.register(exams.models.Assignment)
+class AssignmentAdmin(admin.ModelAdmin):
+	list_display = ('id', 'semester', 'name', 'olympiad', 'due_date',)
+	search_fields = ('olympiad__family', 'name',)
