fa5df5d7bcf5e079d4acd94cc0b9904a84078239
Evan Chen <evanchen@mit.edu>
2017-08-05 18:15:57 -0500

    Add core to settings.py

--- otisweb/settings.py
+++ otisweb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# vim: expandtab
