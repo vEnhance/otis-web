3cebef7130db37a56e9d94483c72b9aa2e4656a9
Evan Chen <evanchen@mit.edu>
2017-08-05 18:21:48 -0500

    Admin interface for core

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from import_export import resources, widgets, fields
+from import_export.admin import ImportExportModelAdmin
+
+import core
 
 # Register your models here.
+
+@admin.register(core.models.Semester)
+class SemesterAdmin(admin.ModelAdmin):
+	list_display = ('name', 'active')
+	search_fields = ('name',)
+@admin.register(core.models.Handout)
+class HandoutAdmin(admin.ModelAdmin):
+	list_display = ('position', 'name', 'code')
+	search_fields = ('name', 'code')
