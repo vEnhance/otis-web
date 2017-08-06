51a9b6cce7566dba306793165f3c669e55ad97aa
Evan Chen <evanchen@mit.edu>
2017-08-05 19:31:52 -0500

    Admin interface for roster

--- roster/admin.py
+++ roster/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+import roster
 
 # Register your models here.
+
+
+class StudentInline(admin.TabularInline):
+	model = roster.models.Student
+	fields = ('user', 'semester', 'current_unit_index',)
+
+@admin.register(roster.models.TA)
+class TAAdmin(admin.ModelAdmin):
+	list_display = ('user', 'student_count',)
+	inlines=(StudentInline,)
+
+@admin.register(roster.models.Student)
+class StudentAdmin(admin.ModelAdmin):
+	list_display = ('user', 'semester', 'assistant', 'current_unit_index',)
