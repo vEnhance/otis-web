bad3b2ffe64f9b73756b3342beb13014cbff3f39
Evan Chen <evanchen@mit.edu>
2017-08-05 20:03:17 -0500

    Semester admin controls exams

--- core/admin.py
+++ core/admin.py
@@ -3,13 +3,20 @@ from import_export import resources, widgets, fields
 from import_export.admin import ImportExportModelAdmin
 
 import core
+import exams
 
 # Register your models here.
+
+class AssignmentInline(admin.TabularInline):
+	model = exams.models.Assignment
+	fields = ('name', 'olympiad', 'due_date',)
+	extra = 0
 
 @admin.register(core.models.Semester)
 class SemesterAdmin(admin.ModelAdmin):
 	list_display = ('name', 'active',)
 	search_fields = ('name',)
+	inlines = (AssignmentInline,)
 
 @admin.register(core.models.Unit)
 class UnitAdmin(admin.ModelAdmin):
