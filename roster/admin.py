75ba3a18cd9468d488cfa10c3a6515a02f0b3f26
Evan Chen <evanchen@mit.edu>
2017-08-05 19:53:46 -0500

    Finish up admin interface

--- roster/admin.py
+++ roster/admin.py
@@ -3,16 +3,33 @@ import roster
 
 # Register your models here.
 
+class ActiveFilter(admin.SimpleListFilter):
+	title = "Current"
+	parameter_name = 'type'
+	def lookups(self, request, model_admin):
+		return (("current", "Current"), ("past", "Past"),)
+	def queryset(self, request, queryset):
+		if self.value() is None:
+			return queryset
+		elif self.value() == "current":
+			return queryset.filter(semester__active=True)
+		elif self.value() == "past":
+			return queryset.filter(semester__active=False)
 
 class StudentInline(admin.TabularInline):
 	model = roster.models.Student
-	fields = ('user', 'semester', 'current_unit_index',)
+	fields = ('user', 'name', 'semester', 'current_unit_index',)
+	readonly_fields = ('user', 'name', 'semester',)
+	extra = 0
 
 @admin.register(roster.models.TA)
 class TAAdmin(admin.ModelAdmin):
-	list_display = ('user', 'name', 'student_count',)
+	list_display = ('name', 'semester', 'user', 'student_count',)
 	inlines=(StudentInline,)
+	list_filter=(ActiveFilter,)
 
 @admin.register(roster.models.Student)
 class StudentAdmin(admin.ModelAdmin):
-	list_display = ('user', 'name', 'semester', 'assistant', 'current_unit_index',)
+	list_display = ('name', 'user', 'semester', 'assistant', 'current_unit_index',)
+	ordering = ('semester', 'name', )
+	list_filter=(ActiveFilter,)
