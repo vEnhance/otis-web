6a8a5e3757fcc0d56a9fc2682120fac13e1b8749
Evan Chen <evanchen@mit.edu>
2017-08-08 17:25:32 -0500

    Admin for students and assistants

--- roster/admin.py
+++ roster/admin.py
@@ -1,4 +1,8 @@
-from django.contrib import admin
+from django.contrib import admin, auth
+from import_export import resources, widgets, fields
+from import_export.admin import ImportExportModelAdmin
+
+import core
 import roster
 
 # Register your models here.
@@ -22,14 +26,36 @@ class StudentInline(admin.TabularInline):
 	readonly_fields = ('user', 'name', 'semester',)
 	extra = 0
 
+class RosterResource(resources.ModelResource):
+	user_name = fields.Field(column_name = 'User Name',
+			attribute = 'user',
+			widget = widgets.ForeignKeyWidget(auth.models.User, 'username'))
+	semester_name = fields.Field(column_name = 'Semester Name',
+			attribute = 'semester',
+			widget = widgets.ForeignKeyWidget(core.models.Semester, 'name'))
+
+class TAIEResource(RosterResource):
+	class Meta:
+		skip_unchanged = True
+		model = roster.models.TA
+		fields = ('id', 'name', 'semester_name', 'user_name',)
+
 @admin.register(roster.models.TA)
-class TAAdmin(admin.ModelAdmin):
+class TAAdmin(ImportExportModelAdmin):
 	list_display = ('name', 'semester', 'user', 'student_count',)
 	inlines=(StudentInline,)
 	list_filter=(ActiveFilter,)
+	resource_class = TAIEResource
+
+class StudentIEResource(RosterResource):
+	class Meta:
+		skip_unchanged = True
+		model = roster.models.Student
+		fields = ('id', 'name', 'semester_name', 'user_name', 'assistant', 'current_unit_index',)
 
 @admin.register(roster.models.Student)
-class StudentAdmin(admin.ModelAdmin):
+class StudentAdmin(ImportExportModelAdmin):
 	list_display = ('name', 'user', 'semester', 'assistant', 'current_unit_index',)
 	ordering = ('semester', 'name', )
 	list_filter=(ActiveFilter,)
+	resource_class = StudentIEResource
