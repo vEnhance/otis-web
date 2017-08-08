9b89b88be3fcb99ff116550516469d80e0ef2e21
Evan Chen <evanchen@mit.edu>
2017-08-08 18:02:33 -0500

    Start work on curriculums

--- roster/views.py
+++ roster/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+import itertools
+
+import core
+import roster
 
 # Create your views here.
+
+def curriculum(request, student_id):
+	student = roster.models.Student.objects.get(id = student_id)
+	units = core.models.Unit.objects.all()
+
+	for name, group in itertools.groupby(units, lambda u : u.name):
+		print name
+		for _ in group:
+			print _.code
+	return HttpResponse(student.name)
