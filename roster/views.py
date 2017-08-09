406e7181560146e92e05a37bc4c94211872a0638
Evan Chen <evanchen@mit.edu>
2017-08-08 21:07:02 -0500

    Make views.py reasonable

--- roster/views.py
+++ roster/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 import itertools
@@ -9,11 +9,13 @@ import roster
 # Create your views here.
 
 def curriculum(request, student_id):
-	student = roster.models.Student.objects.get(id = student_id)
+	student = get_object_or_404(roster.models.Student.objects, id = student_id)
 	units = core.models.Unit.objects.all()
 
 	for name, group in itertools.groupby(units, lambda u : u.name):
 		print name
 		for _ in group:
 			print _.code
-	return HttpResponse(student.name)
+	context = {'title' : 'purr', 'content' : student.name}
+	# return HttpResponse("hi")
+	return render(request, "layout.html", context)
