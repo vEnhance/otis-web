f93e1a2ca40ac173524398045791142f370f5243
Evan Chen <evanchen@mit.edu>
2017-08-12 22:50:45 -0700

    Fix totally broken aux function

--- roster/utils.py
+++ roster/utils.py
@@ -1,7 +1,9 @@
+from django.db.models import Q
+
 def get_visible(user, queryset):
 	"""From a queryset, filter out the students which the user can see."""
 	# Otherwise, do listing
 	if user.is_staff:
-		students = queryset
+		return queryset
 	else:
-		students = queryset.filter(Q(user = request.user) | Q(assistant__user = request.user))
+		return queryset.filter(Q(user = user) | Q(assistant__user = user))
