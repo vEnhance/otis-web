09cdbad4dc3b45f8d3b5f293c63fd218916fae51
Evan Chen <evanchen@mit.edu>
2017-08-13 09:59:44 -0700

    Shorten URL

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
 	url(r'main/([0-9]+)/$', views.main, name='dashboard'),
-	url(r'uploads/([0-9]+)/unit/([0-9]+)/$', views.uploads, name='uploads'),
+	url(r'uploads/([0-9]+)/([0-9]+)/$', views.uploads, name='uploads'),
 	url(r'editfile/(?P<pk>[0-9]+)/$', views.UpdateFile.as_view(), name='editfile'),
 	url(r'deletefile/(?P<pk>[0-9]+)/$', views.DeleteFile.as_view(), name='delfile'),
 	url(r'past/$', views.past, name='past'),
