4e39d3298f6c6e2d753d118cf367c4f77d48e9b2
Evan Chen <evanchen@mit.edu>
2017-08-18 14:06:56 -0700

    Better filepaths for uploaded files

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,27 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import os
 import core.models
 import roster.models
+import datetime
 from django.db import models
 from django.contrib.auth import models as auth
 
+def content_file_name(instance, filename):
+	now = datetime.datetime.now()
+	return os.path.join(instance.category, instance.owner.username,\
+			now.strftime("%Y-%m"), filename)
+
 class UploadedFile(models.Model):
 	"""An uploaded file, for example a transcript or homework solutions."""
-	CHOICES = (("HMWK", "PSet Submission"), ("TRNS", "Transcript"),
-			("NOTE", "Notes / Comments"), ("MISC", "Miscellaneous"))
+	CHOICES = (("psets", "PSet Submission"), ("scripts", "Transcript"),
+			("notes", "Notes / Comments"), ("misc", "Miscellaneous"))
 	benefactor = models.ForeignKey(roster.models.Student,
 			help_text = "The student for which this file is meant")
 	owner = models.ForeignKey(auth.User,
 			help_text = "The user who uploaded the file")
 	category = models.CharField(max_length = 10, choices = CHOICES,
 			help_text = "What kind of file this is")
-	content = models.FileField(help_text = "The file itself")
+	content = models.FileField(help_text = "The file itself",
+			upload_to = content_file_name)
 	unit = models.ForeignKey(core.models.Unit, null = True, blank = True,
 			help_text = "The unit for which this file is associated")
 	created_at = models.DateTimeField(auto_now_add=True)
 	def __str__(self):
-		return self.content.name
+		return os.path.basename(self.content.name)
 	class Meta:
 		ordering = ('-created_at',)
 
