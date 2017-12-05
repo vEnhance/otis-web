14c7802831098109143611e78e393ddefae25ef5
Evan Chen <evanchen@mit.edu>
2017-12-05 09:55:46 -0500

    Add invoice to URL's

--- roster/urls.py
+++ roster/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
 	url(r'curriculum/([0-9]+)/$', views.curriculum, name='currshow'),
 	url(r'advance/([0-9]+)/$', views.advance, name='advance'),
+	url(r'invoice/([0-9]+)/$', views.invoice, name='invoice'),
 	url(r'master-schedule/$', views.master_schedule, name='master-schedule'),
 ]
