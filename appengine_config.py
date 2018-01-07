a548a500369e36443c21db7b3f68f280a811936f
Evan Chen <evanchen@mit.edu>
2018-01-07 05:43:18 -0800

    Hints on GAE standard

--- appengine_config.py
+++ appengine_config.py
@@ -1,4 +1,5 @@
 from google.appengine.ext import vendor
+# This requires you to install requirements.txt in a venv
 vendor.add('venv/lib/python2.7/site-packages')
 
 # https://github.com/jschneier/django-storages/issues/281
