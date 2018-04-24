e93cb645e40ed400a102f4a3a115195c39175b2d
Evan Chen <evanchen@mit.edu>
2018-04-24 07:09:27 -0400

    Require login to download handouts

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseBadRequest
 from hashlib import sha256
+from django.contrib.auth.decorators import login_required
 
 from django.views.generic.list import ListView
 from .models import UnitGroup, Unit
@@ -16,17 +17,19 @@ class UnitGroupListView(ListView):
 	queryset = UnitGroup.objects.all().order_by('subject', 'name')\
 			.prefetch_related('unit_set')
 
-def core_redir(url, hash):
+def _core_redir(url, hash):
 	if sha(url) == hash:
 		# return HttpResponse(url)
 		return redirect(url, permanent=False)
 	else:
 		return HttpResponseBadRequest("400. Wrong hash.")
 
-def unit_problems(self, pk, hash):
+@login_required
+def unit_problems(request, pk, hash):
 	unit = Unit.objects.get(pk=pk)
-	return core_redir(unit.prob_url, hash)
+	return _core_redir(unit.prob_url, hash)
 
-def unit_solutions(self, pk, hash):
+@login_required
+def unit_solutions(request, pk, hash):
 	unit = Unit.objects.get(pk=pk)
-	return core_redir(unit.soln_url, hash)
+	return _core_redir(unit.soln_url, hash)
