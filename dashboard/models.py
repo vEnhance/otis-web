dedfd474dbe96652556949798980968b5862bfa1
Evan Chen <evanchen@mit.edu>
2018-09-26 16:04:16 -0400

    Allow only PDF or text extensions in uploads
    Because images are a royal pain

--- dashboard/models.py
+++ dashboard/models.py
@@ -7,11 +7,26 @@ import roster.models
 import datetime
 from django.db import models
 from django.contrib.auth import models as auth
+from django.forms import ValidationError
 
 def content_file_name(instance, filename):
 	now = datetime.datetime.now()
 	return os.path.join(instance.category, instance.owner.username,\
 			now.strftime("%Y-%m"), filename)
+
+ALLOWED_UPLOAD_TYPES = (
+		'application/pdf',
+		'application/x-latex',
+		'text/x-tex',
+		'text/plain',
+		)
+
+def validate_file_extension(value):
+	content_type = value.file.content_type
+	if not content_type.startswith('text/') and content_type not in ALLOWED_UPLOAD_TYPES:
+		raise ValidationError("File type not accepted: " \
+				"must be PDF or plain TeX/text, "
+				"but got MIME-type " + content_type)
 
 class UploadedFile(models.Model):
 	"""An uploaded file, for example a transcript or homework solutions."""
@@ -26,7 +41,8 @@ class UploadedFile(models.Model):
 	description = models.CharField(max_length = 250, blank = True,
 			help_text = "Optional description of the file")
 	content = models.FileField(help_text = "The file itself",
-			upload_to = content_file_name)
+			upload_to = content_file_name,
+			validators = [validate_file_extension])
 	unit = models.ForeignKey(core.models.Unit, null = True, blank = True,
 			help_text = "The unit for which this file is associated")
 	created_at = models.DateTimeField(auto_now_add=True)
