fda77e8d2150bd5c299c5b69bd2489104ea9baba
Evan Chen <evan@evanchen.cc>
2021-01-21 23:22:54 -0500

    I think it's working! Polish up templates

--- arch/views.py
+++ arch/views.py
@@ -42,8 +42,11 @@ class HintUpdate(RevisionMixin, UpdateView):
 	form_class = forms.HintUpdateFormWithReason
 	def form_valid(self, form):
 		reversion.set_comment(form.cleaned_data['reason'] or form.cleaned_data['content'])
-		return super(UpdateHint, self).form_valid(form)
-
+		return super().form_valid(form)
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['problem'] = self.object.problem
+		return context
 class ProblemUpdate(RevisionMixin, UpdateView):
 	context_object_name = "problem"
 	model = models.Problem
@@ -52,7 +55,11 @@ class ProblemUpdate(RevisionMixin, UpdateView):
 		return reverse_lazy("list_problems", args=(self.object.group.id,))
 	def form_valid(self, form):
 		reversion.set_comment(form.cleaned_data['reason'] or form.cleaned_data['content'])
-		return super(UpdateHint, self).form_valid(form)
+		return super().form_valid(form)
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['group'] = self.object.group
+		return context
 
 class HintCreate(RevisionMixin, CreateView):
 	context_object_name = "hint"
@@ -84,6 +91,12 @@ class ProblemCreate(RevisionMixin, CreateView):
 class HintDelete(RevisionMixin, DeleteView):
 	context_object_name = "hint"
 	model = models.Hint
+	def get_success_url(self):
+		return reverse_lazy("hint_list", args=(self.object.problem.id,))
+
+
 class ProblemDelete(RevisionMixin, DeleteView):
 	context_object_name = "problem"
 	model = models.Problem
+	def get_success_url(self):
+		return reverse_lazy("problem_list", args=(self.object.group.id,))
