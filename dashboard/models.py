bac195840ae1efd0c2648d80f10cde20023540bc
Evan Chen <evan@evanchen.cc>
2021-01-30 13:12:24 -0500

    str rep for problem suggestion model

--- dashboard/models.py
+++ dashboard/models.py
@@ -88,3 +88,6 @@ class ProblemSuggestion(models.Model):
 	resolved = models.BooleanField(help_text = "Whether staff has processed this.", default=False)
 	review_notes = models.TextField(help_text = "Staff notes on reviewing.", blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
+	def __str__(self):
+		return self.student.name + " suggested " + self.source \
+				+ " for " + str(self.unit.group)
