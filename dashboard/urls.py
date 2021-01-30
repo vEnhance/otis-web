f6c35acdb2c2457dba3b262ce9ef28ab4681386d
Evan Chen <evan@evanchen.cc>
2021-01-30 12:46:26 -0500

    Migrate urls in dashboard to better path

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-	url(r'portal/([0-9]+)/$', views.portal, name='portal'),
-	url(r'uploads/([0-9]+)/([0-9]+)/$', views.uploads, name='uploads'),
-	url(r'editfile/(?P<pk>[0-9]+)/$', views.UpdateFile.as_view(), name='editfile'),
-	url(r'deletefile/(?P<pk>[0-9]+)/$', views.DeleteFile.as_view(), name='delfile'),
-	url(r'quasigrader/([0-9]+)/$', views.quasigrader, name='quasigrader'),
-	url(r'idlewarn/$', views.idlewarn, name='idlewarn'),
-	url(r'leaderboard/$', views.leaderboard, name='leaderboard'),
-	url(r'downloads/(?P<pk>[0-9]+)/$', views.DownloadListView.as_view(), name='downloads'),
-	url(r'past/$', views.past, name='past'),
-	url(r'index/$', views.index, name='index'),
+	path(r'portal/<int:student_id>/', views.portal, name='portal'),
+	path(r'uploads/<int:student_id>/<int:unit_id>/', views.uploads, name='uploads'),
+	path(r'editfile/<int:pk>/', views.UpdateFile.as_view(), name='editfile'),
+	path(r'deletefile/<int:pk>/', views.DeleteFile.as_view(), name='delfile'),
+	path(r'quasigrader/<int:num_hours>/', views.quasigrader, name='quasigrader'),
+	path(r'idlewarn/', views.idlewarn, name='idlewarn'),
+	path(r'leaderboard/', views.leaderboard, name='leaderboard'),
+	path(r'downloads/<int:pk>/', views.DownloadListView.as_view(), name='downloads'),
+	path(r'past/', views.past, name='past'),
+	path(r'index/', views.index, name='index'),
 ]
