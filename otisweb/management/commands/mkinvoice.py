9f5d25f6bf9468d365297d3d82eefb777216fa97
Evan Chen <evan@evanchen.cc>
2021-02-23 19:22:08 -0500

    Make invoice command for OTIS

---
+++ otisweb/management/commands/mkinvoice.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+import core.models
+import roster.models
+
+class Command(BaseCommand):
+	help = "Creates invoices for all the students in the active semester without an active invoice"
+
+	def handle(self, *args, **options):
+		semester = core.models.Semester.objects.get(active = True) # crash if > 1 semester
+		students = roster.models.Student.objects.filter(semester = semester, invoice__isnull=True)
+
+		invoices = []
+		for s in students:
+			invoices.append(roster.models.Invoice(student = s, \
+				preps_taught = 2, \
+				hours_taught = 8.4 if (s.track == 'A' or s.track == 'B') else 0, \
+				))
+		print(f"Created {len(invoices)} invoices")
+		roster.models.Invoice.objects.bulk_create(invoices)
