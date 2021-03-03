cca74d9748dabf57840bc2344008d91dccca7608
Evan Chen <evan@evanchen.cc>
2021-03-03 10:26:51 -0500

    Migrate URLS't to path for roster app

--- roster/urls.py
+++ roster/urls.py
@@ -1,19 +1,19 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-	url(r'curriculum/([0-9]+)/$', views.curriculum, name='currshow'),
-	url(r'finalize/([0-9]+)/$', views.finalize, name='finalize'),
-	url(r'advance/([0-9]+)/$', views.advance, name='advance'),
-	url(r'auto-advance/([0-9]+)/([0-9]+)$', views.auto_advance, name='auto-advance'),
-	url(r'invoice/$', views.invoice),
-	url(r'invoice/([0-9]+)/$', views.invoice, name='invoice'),
-	url(r'master-schedule/$', views.master_schedule, name='master-schedule'),
-	url(r'edit-invoice/(?P<pk>[0-9]+)$', views.UpdateInvoice.as_view(), name='edit-invoice'),
-	url(r'inquiry/([0-9]+)$', views.inquiry, name='inquiry'),
-	url(r'inquiry/list/$', views.ListInquiries.as_view(), name='list-inquiry'),
-	url(r'inquiry/edit/(?P<pk>[0-9]+)/$', views.EditInquiry.as_view(), name='edit-inquiry'),
-	url(r'inquiry/approve/(?P<pk>[0-9]+)/$', views.approve_inquiry, name='approve-inquiry'),
-	url(r'inquiry/approve/all/$', views.approve_inquiry_all, name='approve-inquiry-all'),
+	path(r'curriculum/<int:student_id>/', views.curriculum, name='currshow'),
+	path(r'finalize/<int:student_id>/', views.finalize, name='finalize'),
+	path(r'advance/<int:student_id>/', views.advance, name='advance'),
+	path(r'auto-advance/<int:student_id>/<int:unit_id>', views.auto_advance, name='auto-advance'),
+	path(r'invoice/', views.invoice),
+	path(r'invoice/<int:student_id>/', views.invoice, name='invoice'),
+	path(r'master-schedule/', views.master_schedule, name='master-schedule'),
+	path(r'edit-invoice/<int:pk>', views.UpdateInvoice.as_view(), name='edit-invoice'),
+	path(r'inquiry/<int:student_id>', views.inquiry, name='inquiry'),
+	path(r'inquiry/list/', views.ListInquiries.as_view(), name='list-inquiry'),
+	path(r'inquiry/edit/<int:pk>/', views.EditInquiry.as_view(), name='edit-inquiry'),
+	path(r'inquiry/approve/<int:pk>/', views.approve_inquiry, name='approve-inquiry'),
+	path(r'inquiry/approve/all/', views.approve_inquiry_all, name='approve-inquiry-all'),
 ]
