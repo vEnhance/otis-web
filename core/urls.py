69598eb3080ed7bdb2744636ecc3b3af2cb7b272
Evan Chen <evan@evanchen.cc>
2021-03-03 11:10:46 -0500

    Migrate core to `path` URL's

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-	url(r'synopsis/$', views.UnitGroupListView.as_view(), name='synopsis'),
-	url(r'unit/p/(?P<pk>[0-9]+)/(?P<hash>[0-9a-z]+)$', views.unit_problems, name='view_problems'),
-	url(r'unit/s/(?P<pk>[0-9]+)/(?P<hash>[0-9a-z]+)$', views.unit_solutions, name='view_solutions'),
+	path(r'synopsis/', views.UnitGroupListView.as_view(), name='synopsis'),
+	path(r'unit/p/<int:pk>/<str:hash>/', views.unit_problems, name='view_problems'),
+	path(r'unit/s/<int:pk>/<str:hash>/', views.unit_solutions, name='view_solutions'),
 ]
