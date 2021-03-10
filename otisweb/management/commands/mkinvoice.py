b139a1126aa2df7ed7ff42f3a5843255da270d63
Evan Chen <evan@evanchen.cc>
2021-03-10 16:30:36 -0500

    mkinvoice accepts semester count

--- otisweb/management/commands/mkinvoice.py
+++ otisweb/management/commands/mkinvoice.py
@@ -5,6 +5,11 @@ import roster.models
 class Command(BaseCommand):
 	help = "Creates invoices for all the students in the active semester without an active invoice"
 
+	def add_arguments(self, parser):
+		parser.add_argument('preps',
+				type=int,
+				help="Number of preps taught")
+
 	def handle(self, *args, **options):
 		semester = core.models.Semester.objects.get(active = True) # crash if > 1 semester
 		students = roster.models.Student.objects.filter(semester = semester, invoice__isnull=True)
@@ -12,7 +17,7 @@ class Command(BaseCommand):
 		invoices = []
 		for s in students:
 			invoices.append(roster.models.Invoice(student = s, \
-				preps_taught = 2, \
+				preps_taught = options['preps'], \
 				hours_taught = 8.4 if (s.track == 'A' or s.track == 'B') else 0, \
 				))
 		print(f"Created {len(invoices)} invoices")
