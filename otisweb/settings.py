34dd5c5e33214b75f43f38d55e2b4b617ef5de37
Evan Chen <evan@evanchen.cc>
2021-04-04 21:47:08 -0400

    Turn off debug in production
    Now that the website seems pretty stable

--- otisweb/settings.py
+++ otisweb/settings.py
@@ -31,8 +31,7 @@ if not PRODUCTION:
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # not bool(os.getenv('IS_PRODUCTION'))
-# Yeah, screw good practice, I've been fighting for four hours because GAE standard sux
+DEBUG = not bool(os.getenv('IS_PRODUCTION'))
 
 # SECURITY WARNING: App Engine's security features ensure that it is safe to
 # have ALLOWED_HOSTS = ['*'] when the app is deployed. If you deploy a Django
