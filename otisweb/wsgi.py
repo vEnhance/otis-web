031d4810a0c362edfb59df4f97e51170c1fdda10
Evan Chen <evan@evanchen.cc>
2021-06-02 08:31:08 -0400

    Not wsgi

--- otisweb/wsgi.py
+++ otisweb/wsgi.py
@@ -7,15 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-from pathlib import Path
-from dotenv import load_dotenv
 import os
-
-PROJECT_ROOT = Path(__file__).parent.parent.absolute()
-ENV_PATH = PROJECT_ROOT / '.env'
-if ENV_PATH.exists():
-	load_dotenv(ENV_PATH)
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "otisweb.settings")
