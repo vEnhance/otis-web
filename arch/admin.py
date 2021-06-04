87a1b2714a1c9fd44e12013d3b6af5f1b432c9b0
Evan Chen <evan@evanchen.cc>
2021-06-04 16:04:15 -0400

    ARCH admin view hints from problem

--- arch/admin.py
+++ arch/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from reversion.admin import VersionAdmin
 from . import models
 
+class HintInline(admin.TabularInline):
+	model = models.Hint
+	fields = ('number', 'keywords', 'content')
+	extra = 0
+
 @admin.register(models.Problem)
 class ProblemAdmin(VersionAdmin):
 	list_display = ('id', 'puid', 'source', 'description',)
 	search_fields = ('puid', 'source', 'description',)
 	list_per_page = 100
+	inlines = (HintInline,)
 
 @admin.register(models.Hint)
 class HintAdmin(VersionAdmin):
@@ -14,3 +20,5 @@ class HintAdmin(VersionAdmin):
 	search_fields = ('number', 'problem__source', 'keywords', 'content',)
 	autocomplete_fields = ('problem',)
 	list_per_page = 30
+
+
