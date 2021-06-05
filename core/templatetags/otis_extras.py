b043bfb7b60f45fd6ab902d014553eaa75c80ec4
Evan Chen <evan@evanchen.cc>
2021-06-05 09:41:25 -0400

    Create TeX download button

--- core/templatetags/otis_extras.py
+++ core/templatetags/otis_extras.py
@@ -17,6 +17,11 @@ def view_solutions(unit):
 	s = sha(unit.soln_url.encode('utf-8'))
 	return reverse("view-solutions", args=(unit.id, s))
 
+@register.simple_tag
+def view_tex(unit):
+	s = sha(unit.prob_url.encode('utf-8'))
+	return reverse("view-tex", args=(unit.id, s))
+
 @register.filter(name='display_initial_choice')
 def display_initial_choice(field):
 	return ' '.join([ucode for (uid, ucode) in field.field._choices\
--- core/views.py
+++ core/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import redirect
-from django.http import HttpResponseBadRequest, HttpResponseRedirect
-from hashlib import sha256
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
+from django.utils.encoding import smart_str
 from django.contrib.auth.decorators import login_required
-
 from django.views.generic.list import ListView
+from hashlib import sha256
+import os
+import pathlib
+
+
 from .models import UnitGroup, Unit, Semester
 
-from hashlib import sha256
 def sha(value):
 	return sha256(value).hexdigest()
 
@@ -28,6 +31,26 @@ def _core_redir(url, hash):
 def unit_problems(request, pk, hash):
 	unit = Unit.objects.get(pk=pk)
 	return _core_redir(unit.prob_url, hash)
+
+@login_required
+def unit_tex(request, pk, hash):
+	unit = Unit.objects.get(pk=pk)
+	url = unit.prob_url
+	if sha(url.encode('utf-8')) == hash:
+		tex_dir = pathlib.Path(os.getenv(
+			"PROBLEM_TEX_PATH",
+			'/home/evan/Documents/OTIS/TeX/'))
+		# kludge
+		basename = url[url.rindex('/')+1:url.index('?')].replace('.pdf', '.tex')
+		tex_path = tex_dir / (basename[:3] + '-tex-' + basename[4:])
+		with open(tex_path, 'r') as f:
+			content = ''.join(f.readlines())
+		response = HttpResponse(content, content_type='application/tex')
+		response['Content-Disposition']='attachment; filename=' \
+				 + smart_str(basename)
+		return response
+	else:
+		return HttpResponseBadRequest("400. Wrong hash.")
 
 @login_required
 def unit_solutions(request, pk, hash):
