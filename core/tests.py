283b3aa1374df6b32bc62e2b83afe0c5b1c336ee
Evan Chen <evan@evanchen.cc>
2021-08-19 08:35:13 -0700

    More test data

--- core/tests.py
+++ core/tests.py
@@ -1,13 +1,30 @@
-from django.test import TestCase
+from dashboard.models import PSet
+from django.contrib.auth.models import User
+from otisweb.tests import OTISTestCase
+from roster.models import Assistant, Student
 
-from core.models import Semester
+from core.models import Semester, Unit
 
 
-class TestSemester(TestCase):
-	fixtures = ('testdata.yaml', )
+class TestCore(OTISTestCase):
+	@classmethod
+	def setUpTestData(cls: type):
+		assistant = Assistant.objects.create(user=User.objects.get(username='staff'))
+		alice = Student.objects.create(
+			user=User.objects.get(username='alice'),
+			track='C',
+			assistant=assistant,
+			semester=Semester.objects.get(pk=1)
+		)
+		PSet.objects.create(unit=Unit.objects.get(pk=1), student=alice, approved=True)
+		PSet.objects.create(unit=Unit.objects.get(pk=2), student=alice, approved=False)
 
 	def test_semester_url(self):
-		semester1 = Semester.objects.get(pk=1)
-		self.assertEqual(f'/dash/past/{semester1.pk}/', semester1.get_absolute_url())
-		semester2 = Semester.objects.get(pk=2)
-		self.assertEqual(f'/dash/past/{semester2.pk}/', semester2.get_absolute_url())
+		for sem in Semester.objects.all():
+			self.assertEqual(f'/dash/past/{sem.pk}/', sem.get_absolute_url())
+		self.assertGreater(Semester.objects.count(), 0)
+
+	def test_permitted(self):
+		self.assertGetBecomesLoginRedirect('view-problems', 1)
+		self.login('alice')
+		self.assertGet20X('view-problems', 1)
---
+++ otisweb/tests.py
@@ -0,0 +1,61 @@
+from typing import Any
+
+from django.contrib.auth.models import User
+from django.http.response import HttpResponse
+from django.test import TestCase
+from django.test.client import Client
+from django.urls.base import reverse_lazy
+
+
+class OTISTestCase(TestCase):
+	fixtures = ('testdata.yaml', )
+
+	def setUp(self):
+		self.client = Client()
+
+	def assert20X(self, response: HttpResponse):
+		self.assertGreaterEqual(response.status_code, 200)
+		self.assertLess(response.status_code, 300)
+
+	def assert40X(self, response: HttpResponse):
+		self.assertGreaterEqual(response.status_code, 400)
+		self.assertLess(response.status_code, 500)
+
+	def assertDenied(self, response: HttpResponse):
+		self.assertEqual(response.status_code, 403)
+
+	def get(self, name: str, *args: Any):
+		return self.client.get(reverse_lazy(name, args=args))
+
+	def post(self, name: str, *args: Any, **kwargs: Any):
+		return self.client.post(reverse_lazy(name, args=args), kwargs)
+
+	def assertGet20X(self, name: str, *args: Any):
+		self.assert20X(self.get(name, *args))
+
+	def assertGet40X(self, name: str, *args: Any):
+		self.assert40X(self.get(name, *args))
+
+	def assertGetDenied(self, name: str, *args: Any):
+		self.assertDenied(self.get(name, *args))
+
+	def assertPost20X(self, name: str, *args: Any, **kwargs: Any):
+		self.assert20X(self.post(name, *args, **kwargs))
+
+	def assertPost40X(self, name: str, *args: Any, **kwargs: Any):
+		self.assert40X(self.post(name, *args, **kwargs))
+
+	def assertPostDenied(self, name: str, *args: Any, **kwargs: Any):
+		self.assertDenied(self.post(name, *args, **kwargs))
+
+	def login(self, username: str):
+		self.client.force_login(User.objects.get(username=username))
+
+	def assertGetBecomesLoginRedirect(self, name: str, *args: Any):
+		self.assertRedirects(
+			self.get(name, *args), '/accounts/login/?next=' + reverse_lazy(name, args=args)
+		)
+
+	def assertPostBecomesLoginRedirect(self, name: str, *args: Any, **kwargs: Any):
+		redirectURL = '/accounts/login?next=' + reverse_lazy(name, args=args)
+		self.assertRedirects(self.post(name, *args, **kwargs), redirectURL)
