bb7460dbb801ad46d072dab32191755433ca26ca
Evan Chen <evan@evanchen.cc>
2021-09-01 22:48:49 -0700

    feat(arch)!: Concise URL's

--- arch/urls.py
+++ arch/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
 	path(r'<str:puid>/edit/', views.ProblemUpdate.as_view(), name='problem-update'),
 	path(r'<str:puid>/delete/', views.ProblemDelete.as_view(), name='problem-delete'),
 	path(r'<str:puid>/<int:number>/', views.HintDetail.as_view(), name='hint-detail'),
-	path(r'<str:puid>/hints/', views.HintList.as_view(), name='hint-list'),
 	path(r'<str:puid>/add/', views.HintCreate.as_view(), name='hint-create'),
 	path(r'<str:puid>/<int:number>/edit/', views.HintUpdate.as_view(), name='hint-update'),
 	path(r'<str:puid>/<int:number>/delete/', views.HintDelete.as_view(), name='hint-delete'),
+	path(r'<str:puid>/', views.HintList.as_view(), name='hint-list'),
 	path(r'', views.ProblemCreate.as_view(), name='arch-index'),
 ]
