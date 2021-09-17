124d7900ca9f87824ff0b60ed5c5c57ee3787f6b
Evan Chen <evan@evanchen.cc>
2021-09-17 12:03:02 -0400

    fix(otisweb): why the fuck is utf8mb4 not default

--- otisweb/settings.py
+++ otisweb/settings.py
@@ -137,9 +137,12 @@ if os.getenv("DATABASE_NAME"):
 				'PASSWORD': os.getenv("DATABASE_PASSWORD"),
 				'HOST': os.getenv("DATABASE_HOST"),
 				'PORT': os.getenv("DATABASE_PORT", '3306'),
-				'OPTIONS': {
-					'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-				},
+				'OPTIONS':
+					{
+						'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+						'charset': 'utf8mb4',
+						'use_unicode': True,
+					},
 			},
 	}
 else:
