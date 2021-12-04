9918a88b49c686ade093deed92ff02fd3f355e48
Evan Chen <evan@evanchen.cc>
2021-12-04 13:41:56 -0500

    Let's try that again lol

--- mouse/views.py
+++ mouse/views.py
@@ -1,4 +1,5 @@
 from dashboard.models import QuestComplete
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test  # NOQA
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
@@ -19,14 +20,18 @@ def usemo_score(request: HttpRequest) -> HttpResponse:
 			students = Student.objects.filter(semester__active=True)
 			qcs = []
 			for s in students:
-				for line in form.text.split('\n'):
+				for line in form.cleaned_data['text'].splitlines():
+					line = line.strip()
+					if not line:
+						continue
 					name = line[:line.index('\t')].strip()
 					spades = int(line[line.rindex('\t') + 1:].strip())
-					if s.user.get_full_name.lower() == name:
+					if s.user.get_full_name().lower() == name.lower():
 						qcs.append(
 							QuestComplete(student=s, title="USEMO Points", category="US", spades=spades)
 						)
 			QuestComplete.objects.bulk_create(qcs)
+			messages.success(request, f'Built {len(qcs)} records')
 	else:
 		form = ScoreForm()
 
