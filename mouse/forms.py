0b2f4ff00d3366f34cc4cfe8a25d6590ea710f24
Evan Chen <evan@evanchen.cc>
2021-12-04 14:09:49 -0500

    USEMO spades for grader

--- mouse/forms.py
+++ mouse/forms.py
@@ -3,3 +3,7 @@ from django import forms
 
 class ScoreForm(forms.Form):
 	text = forms.CharField(widget=forms.Textarea)
+
+
+class GraderForm(forms.Form):
+	text = forms.CharField(widget=forms.Textarea)
--- mouse/views.py
+++ mouse/views.py
@@ -6,7 +6,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from roster.models import Student
 
-from .forms import ScoreForm
+from .forms import GraderForm, ScoreForm
 
 # Create your views here.
 
@@ -39,4 +39,32 @@ def usemo_score(request: HttpRequest) -> HttpResponse:
 		'title': 'USEMO Score Upload',
 		'form': form,
 	}
-	return render(request, "mouse/scores.html", context)
+	return render(request, "mouse/form.html", context)
+
+
+@login_required
+@user_passes_test(lambda u: u.is_superuser)
+def usemo_grade(request: HttpRequest) -> HttpResponse:
+	if request.method == 'POST':
+		form = GraderForm(request.POST)
+		if form.is_valid():
+			students = Student.objects.filter(semester__active=True)
+			qcs = []
+			for s in students:
+				for line in form.cleaned_data['text'].splitlines():
+					line = line.strip()
+					if not line:
+						continue
+					name = line[:line.index('\t')].strip()
+					if s.user.get_full_name().lower() == name.lower():
+						qcs.append(QuestComplete(student=s, title="USEMO Points", category="UG", spades=15))
+			QuestComplete.objects.bulk_create(qcs)
+			messages.success(request, f'Built {len(qcs)} records')
+	else:
+		form = GraderForm()
+
+	context = {
+		'title': 'USEMO Grader Bounty',
+		'form': form,
+	}
+	return render(request, "mouse/form.html", context)
