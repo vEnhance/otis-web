c24296231e030f0c85175e45fdac3bdfb76803ce
Psi25Omega <76802151+Psi25Omega@users.noreply.github.com>
2022-01-09 09:00:51 +0530

    Update admin.py

--- exams/admin.py
+++ exams/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-from .models import ExamAttempt, Feedback, MockCompleted, PracticeExam
+from .models import ExamAttempt, MockCompleted, PracticeExam
 
 # Register your models here.
 
