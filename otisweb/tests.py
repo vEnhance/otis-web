8e8a98dd50caa9ad8d03e9f27c16674849b61d05
Evan Chen <evan@evanchen.cc>
2022-09-16 00:20:31 -0400

    polish(tests): clean MONKEY PATCH for MONKEY PATCH

--- otisweb/tests.py
+++ otisweb/tests.py
@@ -11,10 +11,11 @@ from django.urls.base import reverse_lazy
 
 factory.random.reseed_random('otisweb')
 
+# OKAY TIME TO MONKEY PATCH THE MONKEY PATCH
 if TYPE_CHECKING:
-	from django.test.client import _MonkeyPatchedWSGIResponse  # type: ignore
+	from django.test.client import _MonkeyPatchedWSGIResponse as MonkeyPatchedWSGIResponse  # type: ignore  # NOQA
 else:
-	_MonkeyPatchedWSGIResponse = Any
+	MonkeyPatchedWSGIResponse = Any
 
 
 # waiting on https://github.com/FactoryBoy/factory_boy/pull/820 ...
@@ -30,30 +31,30 @@ class OTISTestCase(TestCase):
 	def setUp(self):
 		self.client = Client()
 
-	def assert20X(self, response: _MonkeyPatchedWSGIResponse):
+	def assert20X(self, response: MonkeyPatchedWSGIResponse):
 		self.assertGreaterEqual(response.status_code, 200)
 		self.assertLess(response.status_code, 300)
 		return response
 
-	def assertOK(self, response: _MonkeyPatchedWSGIResponse):
+	def assertOK(self, response: MonkeyPatchedWSGIResponse):
 		self.assertLess(response.status_code, 400)
 		return response
 
-	def assert40X(self, response: _MonkeyPatchedWSGIResponse):
+	def assert40X(self, response: MonkeyPatchedWSGIResponse):
 		self.assertGreaterEqual(response.status_code, 400)
 		self.assertLess(response.status_code, 500)
 		return response
 
-	def assertDenied(self, response: _MonkeyPatchedWSGIResponse):
+	def assertDenied(self, response: MonkeyPatchedWSGIResponse):
 		if response.status_code != 400:
 			self.assertEqual(response.status_code, 403)
 		return response
 
-	def assertNotFound(self, response: _MonkeyPatchedWSGIResponse):
+	def assertNotFound(self, response: MonkeyPatchedWSGIResponse):
 		self.assertEqual(response.status_code, 404)
 		return response
 
-	def get(self, name: str, *args: Any, **kwargs: Any) -> _MonkeyPatchedWSGIResponse:
+	def get(self, name: str, *args: Any, **kwargs: Any):
 		if (json_data := kwargs.pop('json', None)) is not None:
 			kwargs['content_type'] = 'application/json'
 			kwargs['data'] = json.dumps(json_data)
@@ -66,34 +67,34 @@ class OTISTestCase(TestCase):
 		return self.client.post(reverse_lazy(name, args=args), follow=True, **kwargs)
 
 	def assertGet20X(self, name: str, *args: Any, **kwargs: Any):
-		return self.assert20X(self.get(name, *args, **kwargs))  # type: ignore
+		return self.assert20X(self.get(name, *args, **kwargs))
 
 	def assertGetOK(self, name: str, *args: Any, **kwargs: Any):
-		return self.assertOK(self.get(name, *args, **kwargs))  # type: ignore
+		return self.assertOK(self.get(name, *args, **kwargs))
 
 	def assertGet40X(self, name: str, *args: Any, **kwargs: Any):
-		return self.assert40X(self.get(name, *args, **kwargs))  # type: ignore
+		return self.assert40X(self.get(name, *args, **kwargs))
 
 	def assertGetDenied(self, name: str, *args: Any, **kwargs: Any):
-		return self.assertDenied(self.get(name, *args, **kwargs))  # type: ignore
+		return self.assertDenied(self.get(name, *args, **kwargs))
 
 	def assertGetNotFound(self, name: str, *args: Any, **kwargs: Any):
-		return self.assertNotFound(self.get(name, *args, **kwargs))  # type: ignore
+		return self.assertNotFound(self.get(name, *args, **kwargs))
 
 	def assertPost20X(self, name: str, *args: Any, **kwargs: Any):
-		return self.assert20X(self.post(name, *args, **kwargs))  # type: ignore
+		return self.assert20X(self.post(name, *args, **kwargs))
 
 	def assertPostOK(self, name: str, *args: Any, **kwargs: Any):
-		return self.assertOK(self.post(name, *args, **kwargs))  # type: ignore
+		return self.assertOK(self.post(name, *args, **kwargs))
 
 	def assertPost40X(self, name: str, *args: Any, **kwargs: Any):
-		return self.assert40X(self.post(name, *args, **kwargs))  # type: ignore
+		return self.assert40X(self.post(name, *args, **kwargs))
 
 	def assertPostDenied(self, name: str, *args: Any, **kwargs: Any):
-		return self.assertDenied(self.post(name, *args, **kwargs))  # type: ignore
+		return self.assertDenied(self.post(name, *args, **kwargs))
 
 	def assertPostNotFound(self, name: str, *args: Any, **kwargs: Any):
-		return self.assertNotFound(self.post(name, *args, **kwargs))  # type: ignore
+		return self.assertNotFound(self.post(name, *args, **kwargs))
 
 	def login_name(self, username: str):
 		user = User.objects.get(username=username)
