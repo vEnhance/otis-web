d081fa28f08d39bb87e25b71639af707ab469338
Evan Chen <evan@evanchen.cc>
2022-09-16 23:40:52 -0400

    tests(wikihaxx): finish wikihaxx tests

--- wikihaxx/tests.py
+++ wikihaxx/tests.py
@@ -1,8 +1,43 @@
 from core.factories import UnitFactory, UserFactory
+from dashboard.factories import AchievementFactory
 from otisweb.tests import OTISTestCase
+
+from wikihaxx.mdx.otis import OTISPreprocessor
 
 from .factories import URLPathFactory
 from .views import WIKI_SUBJECT_CHART, edit_redirect, view_redirect, wiki_redirect  # NOQA
+
+wiki_sample_bbcode = r"""Hello!
+
+Alice says, "you are a doofus".
+
+Bob says, "no you".
+
+Compute the total number of words exchanged.
+
+(For students: the following two codes are for testing. They are not real diamonds.)
+
+[diamond 000000000000000000000000][/diamond]
+
+[diamond 000000000000000000007E57][/diamond]
+
+[generic]
+Name | Evan
+Sex | M
+Nationality | Taiwan
+Known for | EGMO, Napkin, OTIS
+Github | vEnhance
+Instagram | @evanchen.cc
+Twitch | vEnhance
+YouTube | vEnhance
+[/generic]
+
+[unit nonexistent]
+[/unit]
+
+[unit example]
+[/unit]
+"""
 
 
 class WikiTest(OTISTestCase):
@@ -32,3 +67,36 @@ class WikiTest(OTISTestCase):
 		edit_redirect(urlpath)
 		view_redirect(urlpath)
 		wiki_redirect(urlpath)
+
+	def test_preprocessor(self):
+		UnitFactory.create(
+			group__name="Example Unit",
+			group__slug='example',
+			group__subject='M',
+			code='DMW',
+		)
+		AchievementFactory.create(
+			code='000000000000000000007E57',
+			name="Test Diamond",
+			description='Hi.',
+		)
+		AchievementFactory.create(
+			code='100000000000000000007E57',
+			name="Test Diamond with no Image",
+			description='This is to appease coverage branch',
+			image=None
+		)
+
+		p = OTISPreprocessor()
+		reply = p.run(wiki_sample_bbcode.splitlines())
+		self.assertIn(r'Alice says, "you are a doofus".', reply)
+		self.assertIn(r'Bob says, "no you".', reply)
+		self.assertIn(r'<tr class="danger"><th>Code</th><td>INVALID</td></tr>', reply)
+		self.assertIn(r'<tr><th>Name</th><td>Test Diamond</td></tr>', reply)
+		self.assertIn(r'<tr><th>Description</th><td>Hi.</td></tr>', reply)
+		self.assertIn(r'<td>@evanchen.cc</td>', reply)
+		self.assertIn(r'<tr class="danger"><th>Name</th><td>nonexistent</td></tr>', reply)
+		self.assertIn(r'<tr><th>Name</th><td>Example Unit</td></tr>', reply)
+		self.assertIn(r'<tr><th>Classification</th><td>Miscellaneous</td></tr>', reply)
+		self.assertIn(r'<tr><th>Slug</th><td>example</td></tr>', reply)
+		self.assertIn(r'<tr><th>Versions</th><td>DMW</td></tr>', reply)
