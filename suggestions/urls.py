52467ab0e0c35071128b7b58bed06f7f2b44383f
Evan Chen <evan@evanchen.cc>
2022-11-15 11:15:56 -0500

    fix(suggest): missing URL path

--- suggestions/urls.py
+++ suggestions/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
 	path(r'new', views.ProblemSuggestionCreate.as_view(), name='suggest-new'),
 	path(r'<int:unit_id>/new/', views.ProblemSuggestionCreate.as_view(), name='suggest-new'),
 	path(r'<int:pk>/edit/', views.ProblemSuggestionUpdate.as_view(), name='suggest-update'),
+	path(r'list/', views.ProblemSuggestionList.as_view(), name='suggest-list'),
 ]
