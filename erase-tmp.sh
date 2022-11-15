20d8a29db9d8d51cfd677dd36fa7c02fb8d75ef9
Evan Chen <evan@evanchen.cc>
2022-11-15 13:37:03 -0500

    shfmt erase-tmp.sh

--- erase-tmp.sh
+++ erase-tmp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-TO_DELETE=$(ls media/badges/TESTING_*.png media/psets/*/*/TESTING_pset*.txt media/global/*/TESTING_announcement*.txt media/pdfs/TESTING_*.{tex,pdf} 2> /dev/null)
+TO_DELETE=$(ls media/badges/TESTING_*.png media/psets/*/*/TESTING_pset*.txt media/global/*/TESTING_announcement*.txt media/pdfs/TESTING_*.{tex,pdf} 2>/dev/null)
 
 if [ $(echo $TO_DELETE | wc --words) = 0 ]; then
-	echo "No files to delete"
-	exit 1
+  echo "No files to delete"
+  exit 1
 fi
 
 echo $TO_DELETE | tr ' ' '\n' | head -n 8
@@ -13,13 +13,16 @@ echo $TO_DELETE | tr ' ' '\n' | tail -n 8
 echo "$(echo $TO_DELETE | wc --words) files to delete"
 
 function yes_or_no {
-	while true; do
-			read -p "$* [y/n]: " yn
-			case $yn in
-				[Yy]*) return 0  ;;  
-				[Nn]*) echo "Aborted" ; return  1 ;;
-			esac
-	done
+  while true; do
+    read -p "$* [y/n]: " yn
+    case $yn in
+      [Yy]*) return 0 ;;
+      [Nn]*)
+        echo "Aborted"
+        return 1
+        ;;
+    esac
+  done
 }
 
 yes_or_no "Delete?" && echo "nom nom nom" || exit 1
