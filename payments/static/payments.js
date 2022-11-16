0b8064af2af0e75c63544b34a877647823ccb099
Evan Chen <evan@evanchen.cc>
2022-11-15 21:51:24 -0500

    style: Run prettier on JS

--- payments/static/payments.js
+++ payments/static/payments.js
@@ -1,27 +1,37 @@
 fetch("/payments/config/")
-.then((result) => { return result.json(); })
-.then((data) => {
-  // Initialize Stripe.js
-  const stripe = Stripe(data.publicKey);
+  .then((result) => {
+    return result.json();
+  })
+  .then((data) => {
+    // Initialize Stripe.js
+    const stripe = Stripe(data.publicKey);
 
-  // new
-  // Event handler
-  document.querySelector("#payButton").addEventListener("click", () => {
-    // Get Checkout Session ID
-    fetch("/payments/checkout/" + $("#invoice_id").val() + "/" + Math.round($("#amount").val()) + "/")
-    .then((result) => { return result.json(); })
-    .then((data) => {
-      console.log(data);
-      // Redirect to Stripe Checkout
-      return stripe.redirectToCheckout({sessionId: data.sessionId})
-    })
-    .then((res) => {
-      console.log(res);
+    // new
+    // Event handler
+    document.querySelector("#payButton").addEventListener("click", () => {
+      // Get Checkout Session ID
+      fetch(
+        "/payments/checkout/" +
+          $("#invoice_id").val() +
+          "/" +
+          Math.round($("#amount").val()) +
+          "/"
+      )
+        .then((result) => {
+          return result.json();
+        })
+        .then((data) => {
+          console.log(data);
+          // Redirect to Stripe Checkout
+          return stripe.redirectToCheckout({ sessionId: data.sessionId });
+        })
+        .then((res) => {
+          console.log(res);
+        });
     });
   });
-});
 
-document.querySelector("#amount").addEventListener("keypress", function(e) {
+document.querySelector("#amount").addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
     document.querySelector("#payButton").click();
   }
