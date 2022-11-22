fead65ecf064ccda56061454a11b3d46a26434f4
Evan Chen <evan@evanchen.cc>
2022-11-22 09:34:15 -0500

    tests(payments): write the easy tests

--- payments/tests.py
+++ payments/tests.py
@@ -7,11 +7,25 @@ class PaymentTest(EvanTestCase):
 
     def test_invoice_standalone(self):
         alice = StudentFactory.create()
-        invoice = InvoiceFactory.create(student=alice)
-        str(invoice)  # make sure __str__ is alive
+        checksum = alice.get_checksum(settings.INVOICE_HASH_KEY)
 
-        self.assertGet20X('payments-invoice', alice.pk,
-                            alice.get_checksum(settings.INVOICE_HASH_KEY))
         self.assertGetDenied('payments-invoice', alice.pk, '?')
         self.login(alice)
         self.assertGetDenied('payments-invoice', alice.pk, '?')
+
+        self.assertGetNotFound('payments-invoice', alice.pk, checksum)
+
+        invoice = InvoiceFactory.create(student=alice)
+        str(invoice)  # make sure __str__ is alive
+        self.assertGet20X('payments-invoice', alice.pk, checksum)
+
+    def test_config(self):
+        self.assertPost40X('payments-config')
+        resp = self.assertGet20X('payments-config')
+        self.assertIn('publicKey', resp.json())
+
+    def test_success(self):
+        self.assertGet20X('payments-success')
+
+    def test_cancelled(self):
+        self.assertGet20X('payments-cancelled')
