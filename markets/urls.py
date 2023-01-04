cf4434a9dbccafc729ec6e5a89bf0e318d89dc56
coolbluealan <39867758+coolbluealan@users.noreply.github.com>
2023-01-04 18:30:13 -0500

    Add market spades page and update links (#150)
    * Add market spades page and update links
    
    * Change market list wording slightly
    
    * Update market_spades.html
    
    * Update views.py
    
    * punctuation/cosmetic
    
    * test market spade view
    
    Co-authored-by: Evan Chen <evan@evanchen.cc>

--- markets/urls.py
+++ markets/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path(r"results/<str:slug>/", views.MarketResults.as_view(), name="market-results"),
     path(r"list/", views.MarketList.as_view(), name="market-list"),
     path(r"list/past/", views.MarketListPast.as_view(), name="market-list-past"),
+    path(r"spades/", views.MarketSpades.as_view(), name="market-spades"),
     path(r"pending/<int:pk>/", views.GuessView.as_view(), name="market-pending"),
     path(r"recompute/<str:slug>/", views.recompute, name="market-recompute"),
     path(
