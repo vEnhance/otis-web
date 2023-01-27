9cbfcc5c0c4f5455d85ae32ed5a3e3c4fbd50c72
Evan Chen <evan@evanchen.cc>
2023-01-27 10:53:15 -0800

    fix(roster): OK I GIVE UP frikkin mysql migrations

--- roster/migrations/0099_invoice_created_at.py
+++ roster/migrations/0099_invoice_created_at.py
@@ -2,7 +2,6 @@
 
 import datetime
 from django.db import migrations, models
-from django.db.models.expressions import OuterRef, Subquery
 from django.utils.timezone import utc
 
 from typing import Any
@@ -10,13 +9,11 @@ from typing import Any
 
 def set_invoice_created_at_by_student_reg(apps: Any, scheme_editor: Any):
     Invoice = apps.get_model("roster", "Invoice")
-    Invoice.objects.filter(student__reg__isnull=False).update(
-        created_at=Subquery(
-            Invoice.objects.filter(pk=OuterRef("pk")).values(
-                "student__reg__created_at"
-            )[:1]
-        )
-    )
+    invoices = []
+    for invoice in Invoice.objects.filter(student__reg__isnull=False):
+        invoice.created_at = invoice.student.reg.created_at
+        invoices.append(invoice)
+    Invoice.objects.bulk_update(invoices, fields=("created_at",), batch_size=50)
 
 
 class Migration(migrations.Migration):
