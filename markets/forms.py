18e0bcb4953cd7f04c904b6c178162b6ad97102b
Evan Chen <evan@evanchen.cc>
2023-01-30 11:56:46 -0800

    feat(markets): admin button to create new markets

---
+++ markets/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Market
+
+
+class MarketCreateForm(forms.ModelForm):
+    prompt_plain = forms.CharField(widget=forms.Textarea)
+    solution_plain = forms.CharField(widget=forms.Textarea, required=False)
+
+    class Meta:
+        model = Market
+        fields = (
+            "slug",
+            "title",
+            "answer",
+            "show_answer",
+        )
