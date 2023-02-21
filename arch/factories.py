84d9226b6a09c1524a534fe7a31f23f3e0b4f3a3
royce <royce.yao.ry@Gmail.com>
2023-02-20 19:22:46 -0700

    init

--- arch/factories.py
+++ arch/factories.py
@@ -1,17 +1,21 @@
 from factory.declarations import Sequence, SubFactory
 from factory.django import DjangoModelFactory
 from factory.faker import Faker
+from factory.fuzzy import FuzzyInteger
 
-from evans_django_tools.testsuite import UniqueFaker
+from core.factories import UserFactory
 
-from .models import Hint, Problem
+from .models import Hint, Problem, Vote
 
 
 class ProblemFactory(DjangoModelFactory):
     class Meta:
         model = Problem
 
-    puid = UniqueFaker("pystr")
+    # TODO better way of all uppercase
+    puid = Faker(
+        "pystr_format", string_format="??????", letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    )
 
 
 class HintFactory(DjangoModelFactory):
@@ -22,3 +26,12 @@ class HintFactory(DjangoModelFactory):
     number = Sequence(lambda n: n)
     keywords = Faker("job")
     content = Faker("sentence")
+
+
+class VoteFactory(DjangoModelFactory):
+    class Meta:
+        model = Vote
+
+    user = SubFactory(UserFactory)
+    problem = SubFactory(ProblemFactory)
+    niceness = FuzzyInteger(0, 10)
