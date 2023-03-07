e469088ec2fd058a100186fe9564836709fb84ce
Evan Chen <evan@evanchen.cc>
2023-03-06 19:20:08 -0800

    Load/dump script

--- fixtures/dump-all.sh
+++ fixtures/dump-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python manage.py dumpdata --indent 2 --format json -e sessions -e admin -e contenttypes
+python manage.py dumpdata --indent 2 --format json -e sessions -e admin -e contenttypes >fixtures/all.json
