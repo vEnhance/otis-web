fdb13d01c62ce661af6643eba0272080b20d6b1a
Evan Chen <evan@evanchen.cc>
2023-03-06 18:57:57 -0800

    tests(roster): sub-factory for student reg

--- roster/factories.py
+++ roster/factories.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from factory.declarations import LazyAttribute, SubFactory
 from factory.django import DjangoModelFactory
 from factory.faker import Faker
+from factory.fuzzy import FuzzyChoice, FuzzyInteger
 
 from core.factories import SemesterFactory, UnitFactory, UserFactory
 from roster.models import (  # NOQA
@@ -24,12 +25,35 @@ class AssistantFactory(DjangoModelFactory):
     shortname = LazyAttribute(lambda o: o.user.first_name)
 
 
+class RegistrationContainerFactory(DjangoModelFactory):
+    class Meta:
+        model = RegistrationContainer
+
+    semester = SubFactory(SemesterFactory)
+    passcode = Faker("color_name")
+    allowed_tracks = "C,"
+
+
+class StudentRegistrationFactory(DjangoModelFactory):
+    class Meta:
+        model = StudentRegistration
+
+    user = SubFactory(UserFactory)
+    container = SubFactory(RegistrationContainerFactory)
+    parent_email = Faker("ascii_safe_email")
+    track = "C"
+    gender = FuzzyChoice(("M", "F", "H"))
+    graduation_year = FuzzyInteger(2021, 2029)
+    school_name = Faker("city")
+
+
 class StudentFactory(DjangoModelFactory):
     class Meta:
         model = Student
 
     user = SubFactory(UserFactory)
     semester = SubFactory(SemesterFactory)
+    reg = SubFactory(StudentRegistrationFactory)
     track = "C"
     newborn = False
     last_level_seen = 0
@@ -51,25 +75,3 @@ class UnitInquiryFactory(DjangoModelFactory):
     unit = SubFactory(UnitFactory)
     action_type = "INQ_ACT_UNLOCK"
     explanation = Faker("sentence")
-
-
-class RegistrationContainerFactory(DjangoModelFactory):
-    class Meta:
-        model = RegistrationContainer
-
-    semester = SubFactory(SemesterFactory)
-    passcode = Faker("color_name")
-    allowed_tracks = "C,"
-
-
-class StudentRegistrationFactory(DjangoModelFactory):
-    class Meta:
-        model = StudentRegistration
-
-    user = SubFactory(UserFactory)
-    container = SubFactory(RegistrationContainerFactory)
-    parent_email = Faker("ascii_safe_email")
-    track = "C"
-    gender = "H"
-    graduation_year = 0
-    school_name = Faker("city")
