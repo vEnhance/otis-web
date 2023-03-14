c80d1327343e54ddbae85bcedb88a43c0064384a
Evan Chen <evan@evanchen.cc>
2023-03-13 17:54:27 -0700

    edit(opal): HuntList doesn't need to be gated

--- opal/views.py
+++ opal/views.py
@@ -1,11 +1,10 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models.query import QuerySet
 from django.views.generic.list import ListView
 
 from .models import OpalHunt
 
 
-class HuntList(LoginRequiredMixin, ListView[OpalHunt]):
+class HuntList(ListView[OpalHunt]):
     model = OpalHunt
     context_object_name = "hunts"
 
