92392e7c969da16fa66d2e3398f6618d3d45d07b
Incompleteusern <58920010+Incompleteusern@users.noreply.github.com>
2023-05-02 18:50:53 -0700

    Diamond in Wiki Based off pk (#214)
    * hide code
    
    * hide code
    
    * Update otis.py
    
    ---------
    
    Co-authored-by: Evan Chen <evan@evanchen.cc>

--- wikihaxx/mdx/otis.py
+++ wikihaxx/mdx/otis.py
@@ -42,36 +42,43 @@ class OTISPreprocessor(markdown.preprocessors.Preprocessor):
 
                 if tag_name == "diamond":
                     try:
-                        diamond = Achievement.objects.get(code__iexact=tag_arg)
+                        diamond = Achievement.objects.get(pk=tag_arg)
                     except Achievement.DoesNotExist:
                         table_output.append(
-                            '<tr class="danger"><th>Code</th><td>INVALID</td></tr>'
+                            '<tr class="danger"><th>Diamond</th><td>INVALID</td></tr>'
                         )
                     else:
-                        if diamond.image:
-                            art_url = diamond.image.url
-                            output.append(
-                                f'<div class="W-100 text-center"><a href="{art_url}">'
-                                f'<img class="w-100" src="{art_url}" /></a></div>'
-                            )
-                        table_output.append(
-                            f"<tr><th>Name</th><td>{diamond.name}</td></tr>"
-                        )
-                        table_output.append(
-                            f"<tr><th>Value</th><td>{diamond.diamonds}◆</td></tr>"
-                        )
                         num_found = (
                             AchievementUnlock.objects.filter(
                                 achievement=diamond
                             ).count()
                             or 0
                         )
-                        table_output.append(
-                            f"<tr><th>Found by</th><td>{num_found}</td></tr>"
-                        )
-                        table_output.append(
-                            f"<tr><th>Description</th><td>{diamond.description}</td></tr>"
-                        )
+
+                        if diamond.creator is None and num_found > 0:
+                            if diamond.image:
+                                art_url = diamond.image.url
+                                output.append(
+                                    f'<div class="W-100 text-center"><a href="{art_url}">'
+                                    f'<img class="w-100" src="{art_url}" /></a></div>'
+                                )
+                            table_output.append(
+                                f"<tr><th>Name</th><td>{diamond.name}</td></tr>"
+                            )
+                            table_output.append(
+                                f"<tr><th>Value</th><td>{diamond.diamonds}◆</td></tr>"
+                            )
+                            table_output.append(
+                                f"<tr><th>Found by</th><td>{num_found}</td></tr>"
+                            )
+                            table_output.append(
+                                f"<tr><th>Description</th><td>{diamond.description}</td></tr>"
+                            )
+                        else:
+                            table_output.append(
+                                '<tr class="danger"><th>Diamond</th><td>NOT ALLOWED</td></tr>'
+                            )
+
                 elif tag_name == "unit":
                     try:
                         unitgroup = UnitGroup.objects.get(
