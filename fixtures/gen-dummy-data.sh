13fe2123d590d5bd15460ad8f32564fdd38240f5
Evan Chen <evan@evanchen.cc>
2023-07-28 18:14:53 -0700

    feat: fixture sh auto-cd to git root
    Closes #221

--- fixtures/gen-dummy-data.sh
+++ fixtures/gen-dummy-data.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd "$(git rev-parse --show-toplevel)" || exit
 python manage.py migrate
 python manage.py loaddata fixtures/core.UnitGroup.json
 python manage.py loaddata fixtures/core.Unit.json
--- fixtures/load-all.sh
+++ fixtures/load-all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+cd "$(git rev-parse --show-toplevel)" || exit
 python manage.py migrate
 python manage.py loaddata fixtures/all.json
