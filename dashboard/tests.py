37ecbc5a6aaad9bd55812b361a5bf676b0c4f23a
royce <royce.yao.ry@Gmail.com>
2023-08-15 11:38:24 -0700

    test get_news

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 from django.utils import timezone
 from freezegun import freeze_time
 
-from core.factories import SemesterFactory, UnitFactory, UnitGroupFactory, UserFactory
+from core.factories import SemesterFactory, UnitFactory, UnitGroupFactory, UserFactory, UserProfileFactory
 from core.models import Unit
 from dashboard.factories import PSetFactory, SemesterDownloadFileFactory
 from dashboard.models import PSet, UploadedFile
@@ -15,6 +15,7 @@ from dashboard.utils import get_units_to_submit, get_units_to_unlock
 from evans_django_tools.testsuite import EvanTestCase
 from exams.factories import QuizFactory, TestFactory
 from markets.factories import MarketFactory
+from opal.factories import OpalHuntFactory
 from otisweb.utils import get_mailchimp_campaigns
 from roster.factories import (
     AssistantFactory,
@@ -23,6 +24,7 @@ from roster.factories import (
     StudentFactory,
     StudentRegistrationFactory,
 )
+from hanabi.factories import HanabiContestFactory
 from rpg.factories import BonusLevelFactory
 from rpg.models import Level
 
@@ -62,26 +64,32 @@ class TestPortal(EvanTestCase):
 
         PSetFactory.create(student=alice, clubs=501, hours=0, status="A", unit=unit)
 
+        alice_profile = UserProfileFactory.create(user=alice.user)
+        alice_profile.last_notif_dismiss = datetime.datetime(2021, 6, 1, tzinfo=timezone.utc)
+        alice_profile.save()
+
         prevSemester = SemesterFactory.create(end_year=2020)
         StudentFactory.create(user=alice.user, semester=prevSemester)
 
         test = TestFactory.create(
-            start_date=datetime.datetime(2021, 1, 1, tzinfo=timezone.utc),
-            due_date=datetime.datetime(2021, 12, 31, tzinfo=timezone.utc),
+            start_date=datetime.datetime(2021, 6, 1, tzinfo=timezone.utc),
+            due_date=datetime.datetime(2021, 7, 31, tzinfo=timezone.utc),
             family="Waltz",
             number=1,
         )
 
         quiz = QuizFactory.create(
-            start_date=datetime.datetime(2021, 1, 1, tzinfo=timezone.utc),
-            due_date=datetime.datetime(2021, 12, 31, tzinfo=timezone.utc),
+            start_date=datetime.datetime(2021, 6, 1, tzinfo=timezone.utc),
+            due_date=datetime.datetime(2021, 7, 31, tzinfo=timezone.utc),
             family="Waltz",
             number=1,
         )
 
+        # News items
+
         market = MarketFactory.create(
-            start_date=datetime.datetime(2021, 1, 1, tzinfo=timezone.utc),
-            end_date=datetime.datetime(2021, 12, 31, tzinfo=timezone.utc),
+            start_date=datetime.datetime(2021, 6, 30, tzinfo=timezone.utc),
+            end_date=datetime.datetime(2021, 7, 10, tzinfo=timezone.utc),
         )
 
         download = SemesterDownloadFileFactory.create(
@@ -89,18 +97,29 @@ class TestPortal(EvanTestCase):
             created_at=datetime.datetime(2021, 6, 25, tzinfo=timezone.utc),
         )
 
+        hanabi = HanabiContestFactory.create(
+            start_date=datetime.datetime(2021, 6, 30, tzinfo=timezone.utc),
+            end_date=datetime.datetime(2021, 7, 18, tzinfo=timezone.utc)
+        )
+
+        opal = OpalHuntFactory.create(
+            start_date=datetime.datetime(2021, 6, 30, tzinfo=timezone.utc)
+        )
+
         # assistant does not cause level up message
         assistant = AssistantFactory.create()
         alice.assistant = assistant
         alice.save()
         self.login(assistant)
-        with freeze_time("2021-06-30", tz_offset=0):
+        with freeze_time("2021-07-01", tz_offset=0):
             resp = self.assertGet20X("portal", alice.pk, follow=True)
         messages = [m.message for m in resp.context["messages"]]
         self.assertNotIn("You leveled up! You're now level 22.", messages)
 
+        # TODO - check user profile changes to see if they are working?
+
         self.login(alice)
-        with freeze_time("2021-06-30", tz_offset=0):
+        with freeze_time("2021-07-01", tz_offset=0):
             resp = self.assertGet20X("portal", alice.pk, follow=True)
 
         messages = [m.message for m in resp.context["messages"]]
@@ -111,11 +130,26 @@ class TestPortal(EvanTestCase):
         self.assertHas(resp, unit.code)
         self.assertHas(resp, test)
         self.assertHas(resp, quiz)
-        self.assertHas(resp, get_mailchimp_campaigns(14)[0]["summary"])
-        self.assertHas(resp, quiz)
+
+        mail_chimp = get_mailchimp_campaigns(0)[0]
+
+        # check for news notifications
+        self.assertHas(resp, mail_chimp["title"])
         self.assertHas(resp, download.content)
-        self.assertHas(resp, market.slug)
+        self.assertHas(resp, market.title)
+        self.assertHas(resp, hanabi.variant_name)
+        self.assertHas(resp, opal.name)
+
+        self.assertHas(resp, f"New email: {mail_chimp['summary']}")
+        self.assertHas(resp, f"New file: {download.description}")
+        self.assertHas(resp, "New market opened:")
+        self.assertHas(resp, "New Hanabi seed active:")
+        self.assertHas(resp, "OPAL hunt active!")
+
+        # number of clubs
         self.assertHas(resp, 501)
+
+        # TODO check for whether meters are being rendered?
 
 
 # python manage.py test dashboard.tests.TestCertify
---
+++ opal/factories.py
@@ -0,0 +1,10 @@
+from factory.django import DjangoModelFactory
+
+from .models import OpalHunt
+
+class OpalHuntFactory(DjangoModelFactory):
+    class Meta:
+        model = OpalHunt
+
+    name = "Your Otis in April"
+
