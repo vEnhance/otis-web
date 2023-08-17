5fde5f72002004d4cc29ca1f216157e42057adee
Evan Chen <evan@evanchen.cc>
2023-08-16 21:23:03 -0700

    edit(core): optimize last_seen middleware
    this is like 90% of the SQL load time apparently
    
    - only update once every ~15 minutes
    - only update the one changed field, last_seen

--- core/middleware.py
+++ core/middleware.py
@@ -1,5 +1,6 @@
 from typing import Callable
 
+from django.core.cache import cache
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.utils import timezone
@@ -11,11 +12,16 @@ class LastSeenMiddleware:
     def __init__(self, get_response: Callable[[HttpRequest], HttpResponse]):
         self.get_response = get_response
 
+    # following the idea of https://stackoverflow.com/a/57344768/4826845
     def __call__(self, request: HttpRequest):
         response = self.get_response(request)
-        if not request.user.is_authenticated:
+        if not request.user.is_authenticated or not request.session.session_key:
             return response
         up, _ = UserProfile.objects.get_or_create(user=request.user)
-        up.last_seen = timezone.now()
-        up.save()
+        key = f"last-seen-{request.session.session_key}"
+        recently_seen = cache.get(key)
+        if not recently_seen:
+            cache.set(key, 1, 60 * 15)  # we won't update last_seen for 15 minutes
+            up.last_seen = timezone.now()
+            up.save(update_fields=("last_seen",))
         return response
