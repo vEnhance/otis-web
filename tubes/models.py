f4c8f008d6d17c615e29c46c3b3bbcd5ae898308
Evan Chen <evan@evanchen.cc>
2023-11-26 23:41:29 -0500

    style(tubes): remove useless comment

--- tubes/models.py
+++ tubes/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-
 
 class Tube(models.Model):
     """This is a container in which testsolving happens.
