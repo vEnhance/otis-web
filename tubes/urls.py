9c6d9cd4aa6a04053eaecec64d44afc5098852e2
Evan Chen <evan@evanchen.cc>
2023-11-26 23:42:29 -0500

    refactor(tubes): rename views

--- tubes/urls.py
+++ tubes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r"list/", views.TubeList.as_view(), name="tubes-list"),
-    path(r"invite/<int:pk>/", views.tube_join, name="tubes-join"),
+    path(r"list/", views.TubeList.as_view(), name="tube-list"),
+    path(r"invite/<int:pk>/", views.tube_join, name="tube-join"),
 ]
--- tubes/views.py
+++ tubes/views.py
@@ -1,11 +1,9 @@
-from django.contrib import messages
 from django.core.exceptions import PermissionDenied
 from django.db.models.query import QuerySet
 from django.db.models.query_utils import Q
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
-from django.urls.base import reverse
 from django.views.generic.list import ListView
 from sql_util.aggregates import Exists
 
@@ -29,14 +27,12 @@ class TubeList(VerifiedRequiredMixin, ListView[Tube]):
 @verified_required
 def tube_join(request: HttpRequest, pk: int) -> HttpResponse:
     tube = get_object_or_404(Tube, pk=pk)
-    if not tube.is_active:
+    if not tube.status == "TB_ACTIVE":
         raise PermissionDenied("Cannot join inactive tube")
     elif JoinRecord.objects.filter(tube=tube, user=request.user).exists():
-        messages.error(request, "You already joined this.")
-        return HttpResponseRedirect(reverse("tubes-list"))
+        raise PermissionDenied("You already joined this.")
     elif not tube.has_join_url:
-        messages.error(request, "Joining is not currently possible.")
-        return HttpResponseRedirect(reverse("tubes-list"))
+        raise PermissionDenied("Joining not currently allowed.")
     else:
         JoinRecord.objects.create(user=request.user, tube=tube)
         return HttpResponseRedirect(tube.join_url)
