23642664377a820b6fd51d31400be7fed03403eb
Evan Chen <evan@evanchen.cc>
2023-11-26 23:58:52 -0500

    tests(tubes): test tubes haha get it

--- tubes/tests.py
+++ tubes/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from core.factories import GroupFactory, UserFactory
+from evans_django_tools.testsuite import EvanTestCase
 
-# Create your tests here.
+from .factories import TubeFactory
+from .models import JoinRecord
+
+
+class TubeTests(EvanTestCase):
+    def test_active_tubes(self) -> None:
+        tube = TubeFactory.create()
+
+        verified_group = GroupFactory(name="Verified")
+        UserFactory.create(username="mallory")
+        UserFactory.create(username="alice", groups=(verified_group,))
+
+        self.login("mallory")
+        self.assertGet30X("tube-list")
+        resp = self.assertGet30X("tube-join", tube.pk)  # login redirect
+        self.assertTrue(
+            "/accounts/login" in getattr(resp, "url")
+        )  # TODO: seems scuffed af
+        self.assertFalse(JoinRecord.objects.exists())
+
+        self.login("alice")
+        resp = self.assertGet20X("tube-list")
+        self.assertContains(resp, tube.display_name)
+        self.assertNotContains(resp, tube.main_url)
+
+        resp = self.assertGet30X("tube-join", tube.pk)  # redirect to join URL
+        self.assertEqual(len(JoinRecord.objects.all()), 1)
+
+        resp = self.assertGet20X("tube-list")
+        self.assertContains(resp, tube.display_name)
+        self.assertContains(resp, tube.main_url)
+
+        self.assertGet40X("tube-join", tube.pk)
+        self.assertEqual(len(JoinRecord.objects.all()), 1)
+
+    def test_inactive_tubes(self) -> None:
+        tube = TubeFactory.create(status="TB_HIDDEN")
+        verified_group = GroupFactory(name="Verified")
+        UserFactory.create(username="alice", groups=(verified_group,))
+
+        self.login("alice")
+        resp = self.assertGet20X("tube-list")
+        self.assertNotContains(resp, tube.display_name)
+        self.assertGet40X("tube-join", tube.pk)
