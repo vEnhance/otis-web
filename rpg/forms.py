a586b1e55be75953f50ba92795d401229e9a26ee
Evan Chen <evan@evanchen.cc>
2024-01-01 13:52:09 -0800

    feat(rpg): Input validation for diamonds
    Closes #286

--- rpg/forms.py
+++ rpg/forms.py
@@ -2,4 +2,12 @@ from django import forms
 
 
 class DiamondsForm(forms.Form):
-    code = forms.CharField(label="", max_length=64, required=True)
+    code = forms.RegexField(
+        label="Enter a hex code of length 24-26 here.",
+        regex=r"[a-fA-F0-9]{24,26}",
+        required=True,
+        min_length=24,
+        max_length=26,
+        error_messages={"invalid": "This doesn't appear to be a hex code."},
+        widget=forms.TextInput(attrs={"pattern": "[a-fA-F]+"}),
+    )
