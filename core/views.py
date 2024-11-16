9c87e6f308a0aa6d46caab79bfe38f762f912f75
ZVFrozel <zvfrozel@gmail.com>
2024-11-16 18:17:58 +0530

    Require login to view catalog

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from typing import Any, Optional
 
 from braces.views import LoginRequiredMixin, SuperuserRequiredMixin
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.exceptions import PermissionDenied
@@ -40,7 +41,7 @@ class AdminUnitListView(SuperuserRequiredMixin, ListView[Unit]):
     context_object_name = "unit_list"
 
 
-class UnitGroupListView(ListView[Unit]):
+class UnitGroupListView(LoginRequiredMixin, ListView[Unit]):
     model = Unit
     context_object_name = "units"
     template_name = "core/unit_list.html"
