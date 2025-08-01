a7ffd549a1a60cda1d9d5d17d080b0278cce7fc1
Evan Chen <evan@evanchen.cc>
2025-08-01 09:28:05 -0400

    chore: use slug instead of str in URL
    i'm incompetent

--- markets/urls.py
+++ markets/urls.py
@@ -4,13 +4,13 @@ from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
-    path(r"guess/<str:slug>/", views.SubmitGuess.as_view(), name="market-guess"),
-    path(r"results/<str:slug>/", views.MarketResults.as_view(), name="market-results"),
+    path(r"guess/<slug:slug>/", views.SubmitGuess.as_view(), name="market-guess"),
+    path(r"results/<slug:slug>/", views.MarketResults.as_view(), name="market-results"),
     path(r"list/", views.MarketList.as_view(), name="market-list"),
     path(r"list/past/", views.MarketListPast.as_view(), name="market-list-past"),
     path(r"spades/", views.MarketSpades.as_view(), name="market-spades"),
     path(r"pending/<int:pk>/", views.GuessView.as_view(), name="market-pending"),
-    path(r"recompute/<str:slug>/", views.recompute, name="market-recompute"),
+    path(r"recompute/<slug:slug>/", views.recompute, name="market-recompute"),
     path(
         r"", RedirectView.as_view(pattern_name="market-list"), name="market-recompute"
     ),
