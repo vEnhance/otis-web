a8f7850d06d578da1ea2a3329553728b0f0bfde5
Kempu33334 <aatmikpk@gmail.com>
2025-08-22 21:46:38 -0500

    Update 0064_userprofile_sort_units_by_subject.py

--- core/migrations/0064_userprofile_sort_units_by_subject.py
+++ core/migrations/0064_userprofile_sort_units_by_subject.py
@@ -3,7 +3,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("core", "0063_alter_userprofile_dynamic_progress_and_more"),
     ]
