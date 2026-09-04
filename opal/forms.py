a940e9f89698a36dac9ce4cb7bd34d535d82b95f
Evan Chen <evan@evanchen.cc>
2026-09-04 09:43:30 -0400

    polish(opal): make "call in" explicit

--- opal/forms.py
+++ opal/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class AttemptForm(forms.Form):
-    guess = forms.CharField(max_length=128, label="Submit an answer")
+    guess = forms.CharField(max_length=128, label="Call in an answer")
